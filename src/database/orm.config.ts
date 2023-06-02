65c5a044b28aa51516d62b854032f3cbe4a399fa
Nikita Kudinov <kudinov.nikita@gmail.com>
2023-06-02 14:18:39 +0300

    feat: add default env variables to database connection

--- src/database/orm.config.ts
+++ src/database/orm.config.ts
@@ -9,11 +9,11 @@ dotenv.config();
 
 const config: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST || '0.0.0.0',
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_DATABASE || 'iot',
 
   logging: false,
   synchronize: false,
